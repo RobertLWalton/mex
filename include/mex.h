44aade426a7136d4fac90b3c79d01addf327a321
Robert L Walton <walton@acm.org>
2023-06-16 17:25:35 -0400

    update

--- include/mex.h
+++ include/mex.h
@@ -2,7 +2,7 @@
 //
 // File:	mex.h
 // Author:	Bob Walton (walton@acm.org)
-// Date:	Fri Jun 16 07:53:47 EDT 2023
+// Date:	Fri Jun 16 17:25:13 EDT 2023
 //
 // The authors have placed this program in the public
 // domain; they make no warranty and accept no liability
@@ -31,26 +31,23 @@
 namespace mex {
 
 struct instr {
-    min::uns16 op_code, immedA;
-    min::uns32 immedB;
-    min::gen immedC;
+    min::uns16 op_code, regA, regB, regC;
+    min::gen immed;
 };
 
 enum op_code {
     ADD, ADDI,
-    SUB, SUBR, SUBI, SUBRI,
+    SUB, SUB, SUBI, SUBRI,
     MUL, MULI,
-    DIV, DIVR, DIVI, DIVRI,
-    REM, REMR, REMI, REMRI,
+    DIV, DIV, DIVI, DIVRI,
+    REM, REM, REMI, REMRI,
     EXPI,
     JMP,
     JMPEQ, JMPNE, JMPLT, JMPLEQ, JMPGT, JMPGEQ,
     NEG, ABS,
-    PUSH, PUSHI,
-    POP,
-    ASSIGN,
+    ASSIGN, ASSIGNI,
+    BEGIN, END, ITERATE, BREAK,
     NOP
 };
 
 # endif // MEX_H
-
