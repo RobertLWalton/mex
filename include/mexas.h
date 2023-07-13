3ca2b7e3cea5e80f2b3a9042fe8b0cca65879050
Robert L Walton <walton@acm.org>
2023-07-13 18:21:57 -0400

    update

--- include/mexas.h
+++ include/mexas.h
@@ -2,7 +2,7 @@
 //
 // File:	mexas.h
 // Author:	Bob Walton (walton@acm.org)
-// Date:	Thu Jul 13 06:54:57 EDT 2023
+// Date:	Thu Jul 13 14:25:02 EDT 2023
 //
 // The authors have placed this program in the public
 // domain; they make no warranty and accept no liability
@@ -52,32 +52,45 @@ extern mexas::function_stack function_stack;
 
 // Block Stack
 //
-typedef min::packed_vec_insptr<mexas::op_code>
+typedef min::packed_vec_insptr<mex::op_code>
     block_stack;
 extern mexas::block_stack block_stack_stack;
     // Top of stack is BEG... instruction for the last
     // block that has not yet seen its END...
-    // instruction.
+    // instruction.   BEG... instructions are pushed
+    // into stack.  END... instructions check stack
+    // and pop stack.
+
+// Module Stack
+//
+typedef min::packed_vec_insptr<mex::module>
+    module_stack;
+extern mexas::module_stack module_stack_stack;
+    // After module is assembled it is pushed into this
+    // stack, which is therefore a list of modules
+    // in the order they were assembled: most recent
+    // on top.
 
 // Jump List
 //
 struct jump_element
 {
-    min::uns32 previous, next;
     const min::gen target_name;
     min::uns16 jmp_location;
-    min::uns8 lexical_level, depth;
+    min::uns8 lexical_level, minimum_depth;
     min::uns16 stack_length, stack_minimum;
+    min::uns32 next;
 };
 typedef min::packed_vec_insptr<mexas::jump_element>
     jump_list;
 extern mexas::jump_list jump_list;
-    // The jump list is a doubly linked list in the
+    // The jump list is a singly linked list in the
     // jump_list vector, with jump_list[0] being a
     // dummy element that is head of the free list,
     // and jump_list[1} being a dummy element that is
-    // head of the active list.
-
+    // head of the active list.  When elements are
+    // added they are added to the start, so the
+    // list order is newest-first.
 
 } // end mexas namespace
 
